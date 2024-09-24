b0ebac7b658f5c8bd08759592e0711eaf33cf88e
yeachan <dus001228@naver.com>
2024-09-24 15:46:26 +0900

    🚚 rename

--- video_pixabay.py
+++ video_pixabay.py
@@ -9,12 +9,13 @@ load_dotenv()  # .env 파일에서 환경 변수 불러오기
 API_KEY = os.getenv("PIXABAY_API_KEY")
 
 
-def pixabay(text):
+def pixabay(text, tag):
     # 검색할 키워드와 요청 URL 설정 (비디오 검색)
     querys = extract_keywords(text)
 
-    tag = "computer"
     i = 1
+
+    paths = []
     for query in querys:
         url = f'https://pixabay.com/api/videos/?key={API_KEY}&q={query}&lang=ko&category={tag}'  # 언어 파라미터 추가
 
@@ -28,24 +29,18 @@ def pixabay(text):
             video_url = random_video['videos']['medium']['url']
             print(f"비디오 키워드: {query}")
 
-            # 비디오 다운로드
-            video_response = requests.get(video_url)
+            # 비디오 다운로드 및 크기 조정
+            video_response = requests.get(video_url, stream=True)
 
-            # 비디오 저장
+            # 저장할 비디오 파일 경로
             video_filename = f'asset/video_{i}.mp4'
             with open(video_filename, 'wb') as f:
                 f.write(video_response.content)
             print('비디오가 성공적으로 저장되었습니다.')
-
-            # 저장된 비디오를 1080x1536 크기로 조정
-            clip = VideoFileClip(video_filename)
-            resized_clip = clip.resize((1080, 1536))  # LANCZOS 없이 크기만 조정
-
-            # 크기 조정된 비디오 저장
-            output_filename = f'asset/resized_video_{i}.mp4'
-            resized_clip.write_videofile(output_filename, codec='libx264', fps=30)
-            print(f"크기 조정된 비디오가 {output_filename}에 저장되었습니다.")
             i += 1
+            paths.append(video_filename)
+
         else:
             print(f'{query} 비디오를 찾을 수 없습니다.')
 
+    return paths

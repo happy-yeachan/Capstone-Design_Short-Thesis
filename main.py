3d9d16cddc0dd434e4ddde7993f293ace156d080
yeachan <dus001228@naver.com>
2024-09-24 15:47:53 +0900

    ✨ FastAPI 연동

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
-
+from TTS import tts
+from video_main import merge_videos_with_duration
+from video_pixabay import pixabay
+from video_caption import add_automatic_subtitles
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
+@app.post("/test")
+async def root(text: str, tag: str):
+    tts(text)
+    paths = pixabay(text, tag)
+    merge_videos_with_duration(paths)
+    add_automatic_subtitles(text)
+
     return {"message": "Hello World"}

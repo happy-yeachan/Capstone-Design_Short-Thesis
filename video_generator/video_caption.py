928e8dba0f4ac369276c83c251a694b80d1075ef
LeeJungHoon <dksldsk@naver.com>
2024-10-09 21:10:01 +0900

    ✨파일 삭제 로직 제거

--- video_generator/video_caption.py
+++ video_generator/video_caption.py
@@ -2,14 +2,6 @@ from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip, AudioFil
 import os
 import shutil
 
-def clean_asset_folder(directory="asset"):
-    # asset 디렉토리가 있는지 확인
-    if os.path.exists(directory):
-        # 디렉토리 내 파일을 모두 삭제
-        shutil.rmtree(directory)
-        print(f"{directory} 폴더의 모든 파일을 삭제했습니다.")
-    else:
-        print(f"{directory} 폴더가 존재하지 않습니다.")
 
 def get_next_video_number(directory):
     # 디렉토리 내 파일 목록을 가져오기
@@ -77,8 +69,6 @@ def add_caption(script, tag):
 
     # 최종 비디오 저장 (ex: 5.mp4)
     final_video.write_videofile(f"{output_directory}/{next_number}.mp4", fps=24)
-
-    clean_asset_folder()
     
     return f"{output_directory}/{next_number}.mp4"
 
